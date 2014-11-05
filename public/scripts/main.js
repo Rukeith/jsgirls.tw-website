b5489ac50b7ad0dd3649d5bda2854b356295086a
Your Name <catxii@gmail.com>
2014-11-05 21:27:55 +0800

    修改不輪流播放背景圖

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -87,9 +87,7 @@ $(document).ready(function(){
 // ***********************************								  
 			 
 $("body").backstretch([
-   "images/bg1.jpg",
-   "images/bg2.jpg",
-   "images/bg3.jpg"
+   "images/bg1.jpg"
 ], {duration: 5000, fade: 1000});
 							  
 
